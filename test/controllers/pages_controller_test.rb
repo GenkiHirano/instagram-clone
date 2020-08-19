284d817285d82b74feb9ed3e1d1ee35755a7b262
GenkiHirano <genki_rad@ezweb.ne.jp>
2020-08-19 22:53:03 +0900

    Add a full_title helper

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,32 +2,28 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Instagram"
-  end
-
   test "should get top" do
     get pages_top_url
     assert_response :success
-    assert_select "title", "Top | #{@base_title}"
+    assert_select "title", "Instagram"
   end
 
   test "should get notification" do
       get pages_notification_url
       assert_response :success
-      assert_select "title", "Notification | #{@base_title}"
+      assert_select "title", "Notification | Instagram"
   end
 
   test "should get post" do
       get pages_post_url
       assert_response :success
-      assert_select "title", "Post | #{@base_title}"
+      assert_select "title", "Post | Instagram"
   end
 
   test "should get my_page" do
       get pages_my_page_url
       assert_response :success
-      assert_select "title", "My_page | #{@base_title}"
+      assert_select "title", "My_page | Instagram"
   end
 
 end
